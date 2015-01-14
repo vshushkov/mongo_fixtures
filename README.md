976e9ef801836ec362ba19a4feded1fb4b2af0eb
Vladimir Shushkov <vladimir@shushkov.ru>
2015-01-14 20:11:59 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# mongo_fixtures
+
+A library for Dart developers. It is awesome.
+
+## Usage
+
+A simple usage example:
+
+    import 'package:mongo_fixtures/mongo_fixtures.dart';
+
+    main() {
+      var awesome = new Awesome();
+    }
+
+## Features and bugs
+
+Please file feature requests and bugs at the [issue tracker][tracker].
+
+[tracker]: http://example.com/issues/replaceme
---
+++ lib/mongo_fixtures.dart
@@ -0,0 +1,110 @@
+
+library mongo_fixtures;
+import 'dart:async';
+import 'package:mongo_dart/mongo_dart.dart';
+
+///
+class MongoFixtureLoader {
+
+  ///
+  final String _connectionString;
+
+  ///
+  const MongoFixtureLoader(this._connectionString);
+
+  ///
+  Future _cleanAll(Db db) {
+    return db.listCollections().then((List<String> collections) =>
+      Future.forEach(collections, (String name) =>
+        db.dropCollection(name))
+    );
+  }
+
+  ///
+  Future _insert(Db db, List<MongoFixtureEntity> data) {
+    Map<String, DbCollection> collections = {};
+    return Future.forEach(data, (MongoFixtureEntity entity) {
+      if (!collections.containsKey(entity.collectionName)) {
+        collections[entity.collectionName] = db.collection(entity.collectionName);
+      }
+      return collections[entity.collectionName].insertAll(entity.documents);
+    });
+  }
+
+  ///
+  Future cleanAllAndInsert(List<MongoFixtureEntity> data) {
+    Db db = new Db(_connectionString);
+    return db.open()
+      .then(($) => this._cleanAll(db))
+      .then(($) => this._insert(db, data))
+      .then(($) => db.close());
+  }
+
+  ///
+  Future cleanAll() {
+    Db db = new Db(_connectionString);
+    return db.open()
+    .then(($) => this._cleanAll(db))
+    .then(($) => db.close());
+  }
+
+  ///
+  Future insert(List<MongoFixtureEntity> data) {
+    Db db = new Db(_connectionString);
+    return db.open()
+    .then(($) => this._insert(db, data))
+    .then(($) => db.close());
+  }
+
+}
+
+///
+abstract class MongoFixtureEntity {
+
+  ///
+  static String getId(String documentName) {
+    return null;
+  }
+
+  ///
+  String get collectionName;
+
+  ///
+  List<Map> get documents;
+}
+
+///
+class MongoFixtureDocument implements MongoFixtureEntity {
+
+  @override
+  final String collectionName;
+
+  ///
+  final Map map;
+
+  ///
+  const MongoFixtureDocument({this.collectionName, this.map});
+
+  @override
+  List<Map> get documents => [this.map];
+}
+
+///
+class MongoFixtureCollection implements MongoFixtureEntity {
+
+  List<Map> _documents = [];
+
+  @override
+  final String collectionName;
+
+  ///
+  MongoFixtureCollection(this.collectionName);
+
+  @override
+  List<Map> get documents => this._documents;
+
+  ///
+  void insert(Map map) {
+    this._documents.add(map);
+  }
+}
